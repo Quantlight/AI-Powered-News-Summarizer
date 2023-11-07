54c9e2b13d43615356e5fe1eae86ca57046b94eb
Quantlight <shubhmistry0@gmail.com>
2023-11-07 12:57:19 +0530

    Added comments and Improved Readability

--- static/script.js
+++ static/script.js
@@ -1,23 +1,31 @@
+// Define variables for key elements
 const themeToggle = document.getElementById('theme-toggle');
 const themeLight = document.getElementById('theme-light');
 const themeDark = document.getElementById('theme-dark');
 const themeCustom = document.getElementById('theme-custom');
 const body = document.body;
-let currentTheme = localStorage.getItem('theme') || 'light';
 
-// Function to convert Markdown to HTML
-function convertMarkdownToHTML(markdown) {
-    return marked(markdown);
-}
+// Retrieve current theme from local storage or default to 'light'
+let currentTheme = localStorage.getItem('theme')|| 'light';
 
+// Function to set theme
 function setTheme(theme) {
+    // Remove any previous theme classes
     body.classList.remove('light-theme', 'dark-theme', 'custom-theme');
+
+    // Add the chosen theme class
     body.classList.add(theme);
+
+    // Update the current theme in local storage
     localStorage.setItem('theme', theme);
+
+    // Update the currentTheme variable
     currentTheme = theme;
 }
 
+// Event listener for themeToggle click
 themeToggle.addEventListener('click', () => {
+    // Cycle through possible theme choices
     if (currentTheme === 'light') {
         setTheme('dark-theme');
     } else if (currentTheme === 'dark-theme') {
@@ -29,20 +37,15 @@ themeToggle.addEventListener('click', () => {
     }
 });
 
+// Event listeners to set specific themes on button click
 themeLight.addEventListener('click', () => setTheme('light-theme'));
 themeDark.addEventListener('click', () => setTheme('dark-theme'));
 themeCustom.addEventListener('click', () => setTheme('custom-theme'));
 
+// Set the initial theme when the page loads
 setTheme(currentTheme);
 
-// Replace the content of elements with class "markdown-content" with the converted HTML
-document.querySelectorAll('.markdown-content').forEach((element) => {
-    const markdown = element.textContent; // Get the Markdown content
-    const html = convertMarkdownToHTML(markdown); // Convert to HTML
-    element.innerHTML = html; // Replace the content with HTML
-});
-
-//  Materialize’s custom select elements require initialization before they function properly
+// Initialize Materialize's custom select elements
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems);
